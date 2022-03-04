60bfcd48f7bd75cbbb0f2ead5c992300da9fd8c5
Dhaesha M <dhaesha1@gmail.com>
2022-03-04 10:55:24 -0500

    preparing to render my first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import react  from 'react'
+import "./App.css";
+import { ChakraProvider, Container, Heading } from "@chakra-ui/react";
+
+// import { Provider } from "react-redux";
+// import store from "./store";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <ChakraProvider>
+          <Container maxW="container.lg.lg" centerContent>
+            <Heading as="h1" size="xl">
+              {" "}
+              Snakey Snake Game{" "}
+            </Heading>
+            {/* future children   */}
+          </Container>
+        </ChakraProvider>
+      </Provider>
     </div>
   );
 }
